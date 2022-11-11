1ebd3f9bd7542bebe0d62fe8c955450a1ef837f1
Dmitriy Ryzhov <superdiman_1994@mail.ru>
2022-11-11 09:11:15 +0800

    Добавил отображение списка квартир

--- lib/screens/notes/NoteApartmentsScreen.dart
+++ lib/screens/notes/NoteApartmentsScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:beamer/beamer.dart';
+import 'package:rent_application/models/ApartmentModel.dart';
 import 'package:rent_application/models/HomePhoneModel.dart';
 import 'package:rent_application/repository/firestore_srevice.dart';
 import 'package:rent_application/screens/notes/AddApartmentsForm.dart';
@@ -27,6 +28,31 @@ class NoteApartmentsScreen extends StatefulWidget {
   State<StatefulWidget> createState() => NoteApartmentsScreenState();
 }
 
+//функция преобразования списка снапшотов коллекции в список сообщений
+StreamTransformer<QuerySnapshot<Map<String, dynamic>>, List<ApartmentModel>>
+    documentToApartmentsTransformer = StreamTransformer<
+            QuerySnapshot<Map<String, dynamic>>,
+            List<ApartmentModel>>.fromHandlers(
+        handleData: (QuerySnapshot<Map<String, dynamic>> snapShot,
+            EventSink<List<ApartmentModel>> sink) {
+  List<ApartmentModel> result = [];
+  snapShot.docs.forEach((element) {
+    FirestoreService.getApartments(element.id).then((value) {
+      if (value != null) {
+        result.add(ApartmentModel(
+          address: value['address'],
+          number: value['number'],
+          mainPhoto: value['mainPhoto'],
+          validPhoto: value['validPhoto'],
+        ));
+        sink.add(result = List.from(result.reversed));
+        print('Результат $result');
+      }
+    });
+  });
+  sink.add(result = List.from(result.reversed));
+});
+
 /// The state for DetailsScreen
 class NoteApartmentsScreenState extends State<NoteApartmentsScreen> {
   @override
@@ -35,7 +61,28 @@ class NoteApartmentsScreenState extends State<NoteApartmentsScreen> {
       appBar: AppBar(
         title: Text('Квартиры - Список квартир'),
       ),
-      body: _emptyMessage(),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('apartments')
+              .snapshots()
+              .transform(documentToApartmentsTransformer),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data != null) {
+                return _streamChatsWidget(context, snapshot.data);
+              } else {
+                return _emptyMessage();
+              }
+            }
+            if (snapshot.hasError) {
+              return Text('Произошла ошибка загрузки: ${snapshot.error}');
+            }
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           DocumentReference ref =
@@ -56,6 +103,47 @@ class NoteApartmentsScreenState extends State<NoteApartmentsScreen> {
       ),
     );
   }
+}
+
+Widget _streamChatsWidget(context, List<ApartmentModel> apartmetnsList) {
+  if (apartmetnsList.isEmpty) {
+    return _emptyMessage();
+  } else
+    return ListView.builder(
+        itemCount: apartmetnsList.length,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            child: Card(
+              child: Column(children: [
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: apartmetnsList[index].mainPhoto == null ||
+                                  apartmetnsList[index].mainPhoto == ''
+                              ? NetworkImage(
+                                  'https://careappointments.com/wp-content/uploads/2018/10/no_image_placeholder.png')
+                              : NetworkImage(apartmetnsList[index].mainPhoto))),
+                ),
+                Text(
+                  'Квартира ${apartmetnsList[index].number}',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Адрес: ${apartmetnsList[index].address}',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+              ]),
+            ),
+          );
+        });
 }
 
 Widget _emptyMessage() {
